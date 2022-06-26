357761636ae78148d7c505777fa4742caabcea26
amanda <amandapereiradevcontact@gmail.com>
2022-06-26 16:31:35 -0300

    adicicao de textos

---
+++ classes/classes.php
@@ -0,0 +1,19 @@
+<?php
+//definindo classes em php
+
+
+class contaBancaria
+{
+    $banco;
+    $nomeTitular;
+    $numeroAgencia;
+    $numeroConta;
+    $saldo;
+
+}
+
+
+$conta = new contaBancaria(); // criei uma instancia da classe (objeto)
+
+var_dump($conta);
+?>
