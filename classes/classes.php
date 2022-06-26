f1c3226c92cbe03c3ae455548815fd365b23fbf0
amanda <amandapereiradevcontact@gmail.com>
2022-06-26 17:00:47 -0300

    adicao de textos

--- classes/classes.php
+++ classes/classes.php
@@ -3,17 +3,23 @@
 
 
 class contaBancaria
+
 {
-    $banco;
-    $nomeTitular;
-    $numeroAgencia;
-    $numeroConta;
-    $saldo;
+    public $banco = "Inter";
+    public $nomeTitular = "Amanda da Conceição Pereira";
+    public $numeroAgencia = "3456";
+    public $numeroConta = "242340";
+    public $saldo = "R$500,00";
 
 }
 
 
 $conta = new contaBancaria(); // criei uma instancia da classe (objeto)
 
+
+//acessando o objeto
 var_dump($conta);
+
+//acessando parte do objeto
+var_dump($conta->banco);
 ?>
